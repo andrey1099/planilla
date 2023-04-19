46c740a7f28fba2feacfb62b61759ca57b87ecc5
andrey1099 <131296543+andrey1099@users.noreply.github.com>
2023-04-19 13:04:56 -0600

    Add files via upload

---
+++ interaccion.cpp
@@ -0,0 +1,108 @@
+#include "empleado.h"
+#include "interaccion.h"
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+void interaccion::generacionEmpleados(empleado arrayEmpleados[])
+{
+	int arrayTamano = sizeof(arrayEmpleados) / sizeof(arrayEmpleados[0]);
+
+	for (int i = 0; i < arrayTamano; i++)
+	{
+		char[20] cedulaAux;
+		char[50] nombreAux;
+		float salarioBaseAux = 0;
+
+		cout << "Digite la cedula del empleado #"i + 1": ";
+		cin.getline(cedulaAux, 20);
+		cout << endl << endl;
+
+		cout << "Digite el nombre del empleado #"i + 1": ";
+		cin.getline(nombreAux, 20);
+		cout << endl << endl;
+
+		cout << "Digite el salario base del empleado #"i + 1": ";
+		cin << salarioBaseAux;
+		cout << endl << endl;
+
+		arrayEmpleados[i] = empleado(cedulaAux, nombreAux, salarioBaseAux, i + 1);
+
+		cout << "Empleado #"i + 1" creado";
+
+		system("cls");
+	}
+}
+
+void interaccion::modificarEmpleado(empleado arrayEmpleados[])
+{
+	char[20] cedulaAux;
+	float aumentoSalarial = 0;
+	int arrayTamano = sizeof(arrayEmpleados) / sizeof(arrayEmpleados[0]);
+	cout << "Digite la cedula del empleado que desea modificar: ";
+	cin.getline(cedulaAux, 20);
+	cout << endl << endl;
+
+	for (int i = 0; i < arrayTamano; i++)
+	{
+		if (strcmp(cedulaAux, arrayEmpleados[i].getCedula) == 0) {
+
+			cout << "Digite el aumento salarial que desea hacer: ";
+			cin >> aumentosalarial;
+
+			arrayEmpleados[i].aumentarSalarioActual(aumentoSalarial);
+
+			cout << "aumento salarial realizado";
+		}
+	}
+}
+
+void interaccion::imprimirEmpleados(empleado arrayEmpleados[])
+{
+	int arrayTamano = sizeof(arrayEmpleados) / sizeof(arrayEmpleados[0]);
+
+	for (int i = 0; i < arrayTamano; i++)
+	{
+		arrayEmpleados[i].imprimirEmpleado;
+
+		cout << endl << endl;
+	}
+}
+
+void interaccion::pagarEmpleados(empleado arrayEmpleados[])
+{
+	int arrayTamano = sizeof(arrayEmpleados) / sizeof(arrayEmpleados[0]);
+
+	for (int i = 0; i < arrayTamano; i++)
+	{
+		arrayEmpleados[i].registrarPagoSalario;
+
+		cout << endl << endl;
+	}
+
+	cout << "Pagos realizados a todos los empleados";
+}
+
+void interaccion::obtenerAguinaldo(empleado arrayEmpleados[])
+{
+	char[20] cedulaAux;
+
+	float aguinaldoEmpleado = 0;
+	int arrayTamano = sizeof(arrayEmpleados) / sizeof(arrayEmpleados[0]);
+	cout << "Digite la cedula del empleado para obtener el aguinaldo a pagar: ";
+	cin.getline(cedulaAux, 20);
+	cout << endl << endl;
+
+	for (int i = 0; i < arrayTamano; i++)
+	{
+		if (strcmp(cedulaAux, arrayEmpleados[i].getCedula) == 0) {
+
+			aguinaldoEmpleado = arrayEmpleados[i].obtenerAguinaldo();
+
+			cout << "El aguinaldo del empleado encontrado es: "aguinaldoEmpleado;
+		}
+	}
+}
+
---
+++ interaccion.h
@@ -0,0 +1,16 @@
+#include "empleado.h"
+
+class interaccion
+{
+
+	private:
+
+
+	public:
+
+		void generacionEmpleados(empleado[]);
+		void modificarEmpleado(empleado[]);
+		void imprimirEmpleados(empleado[]);
+		void pagarEmpleados(empleado[]);
+		void obtenerAguinaldo(empleado[]);
+};
